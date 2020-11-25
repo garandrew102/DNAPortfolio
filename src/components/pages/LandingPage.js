069598ca43adba53cdebf0c6b2a59f2d8b6d3286
garandrew102 <garandrew102@gmail.com>
2020-11-25 17:30:28 -0500

    created pages and footer

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../../App.css";
 import Home from "../Home";
 import Cards from "../Cards";
+import Footer from "../Footer";
 
 function LandingPage() {
   return (
     <>
       <Home />
       <Cards />
+      <Footer />
     </>
   );
 }
