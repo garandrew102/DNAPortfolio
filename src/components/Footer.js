ac72056f5b0de878f73b29f91774a2e8a7c0a51e
garandrew102 <garandrew102@gmail.com>
2020-11-25 18:04:21 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -64,11 +64,11 @@ function Footer() {
         <div class="social-media-wrap">
           <div class="footer-logo">
             <Link to="/" className="social-logo">
-              TRVL
+              DNA
               <i class="fab fa-typo3" />
             </Link>
           </div>
-          <small class="website-rights">TRVL © 2020</small>
+          <small class="website-rights">DNA © 2020</small>
           <div class="social-icons">
             <Link
               class="social-icon-link facebook"
@@ -103,7 +103,7 @@ function Footer() {
               <i class="fab fa-twitter" />
             </Link>
             <Link
-              class="social-icon-link twitter"
+              class="social-icon-link linkedin"
               to="/"
               target="_blank"
               aria-label="LinkedIn"
