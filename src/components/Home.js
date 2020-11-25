a107c5a212df2bc297c3b914ce7157d38acd0432
garandrew102 <garandrew102@gmail.com>
2020-11-25 18:18:37 -0500

    tested other video and renamed file

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Button } from "./Button";
 import "../App.css";
 import "./Home.css";
-import video2 from "../videos/video2.mp4";
+import video1 from "../videos/video1.mp4";
 
 function Home() {
   return (
     <div className="hero-container">
       <video autoPlay loop muted>
-        <source src={video2} type="video/mp4" />
+        <source src={video1} type="video/mp4" />
       </video>
 
       <h1>ADVENTURE AWAITS</h1>
