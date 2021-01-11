e6c24825a0602fd7eee80f0ccc9bfdc91862e05b
garandrew102 <garandrew102@gmail.com>
2021-01-10 23:30:55 -0500

    updated contact page

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,13 +1,130 @@
-import React from "react";
-import Form from "../../Form/Form";
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+import video1 from "../../../videos/video1.mp4";
+import Axios from "axios";
+import "../../Button/Button.css";
 import "./Contact.css";
 
-export default function Contact() {
-  return (
-    <div>
-      <h1>
-        <Form />
-      </h1>
-    </div>
-  );
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      disabled: false,
+      emailSent: null,
+    };
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      disabled: true,
+    });
+
+    Axios.post("http://localhost:3030/api/email", this.state)
+      .then((res) => {
+        if (res.data.success) {
+          this.setState({
+            disabled: true,
+            emailSent: true,
+          });
+        } else {
+          this.setState({
+            disabled: false,
+            emailSent: false,
+          });
+        }
+      })
+      .catch((err) => {
+        this.setState({
+          disabled: false,
+          emailSent: false,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="contact">
+        <video autoPlay loop muted>
+          <source src={video1} type="video/mp4" />
+        </video>
+
+        <div className="form">
+          <h1>Let's Connect</h1>
+          <Form onSubmit={this.handleSubmit}>
+            <div className="name">
+              <Form.Group>
+                <Form.Label htmlFor="full-name">Name:</Form.Label>
+                <Form.Control
+                  id="full-name"
+                  name="name"
+                  type="text"
+                  placeholder="Name Here"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+            </div>
+            <div className="email">
+              <Form.Group>
+                <Form.Label htmlFor="email">Email:</Form.Label>
+                <Form.Control
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Email Here"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+            </div>
+            <div className="message">
+              <Form.Group>
+                <Form.Label htmlFor="message">Message:</Form.Label>
+                <Form.Control
+                  id="message"
+                  name="message"
+                  as="textarea"
+                  rows="5"
+                  value={this.state.message}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+            </div>
+            <div className="button">
+              <Button
+                variant="primary"
+                type="submit"
+                disabled={this.state.disabled}
+              >
+                SUBMIT
+              </Button>
+            </div>
+            {this.state.emailSent === true && (
+              <p className="success-msg">Email Sent!</p>
+            )}
+            {this.state.emailSent === false && (
+              <p className="error-msg">Email Not Sent!</p>
+            )}
+          </Form>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Contact;
