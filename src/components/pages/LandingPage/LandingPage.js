fb8390af968cd05d794b66db0a3f5e4d29071119
garandrew102 <garandrew102@gmail.com>
2021-01-10 20:58:30 -0500

    edit landing page and button

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./LandingPage.css";
-import Home from "../../Home/Home";
+import video1 from "../../../videos/video1.mp4";
+import Button from "../../Button/Button";
 import Footer from "../../Footer/Footer";
 
 function LandingPage() {
   return (
     <>
-      <Home />
+      <div className="landing-page">
+        <video autoPlay loop muted>
+          <source src={video1} type="video/mp4" />
+        </video>
+
+        <h1>DNA</h1>
+        <p>Bring Your Idea To Life</p>
+        <div className="landing-page-btns">
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+          >
+            CONTACT
+          </Button>
+        </div>
+      </div>
       <Footer />
     </>
   );
